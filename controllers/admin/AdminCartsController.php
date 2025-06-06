ba8ddce10eeb5a99ac954e19c1d335247c999f98
vishalsingh.qlo325 <vishalsingh.qlo325@webkul.in>
2025-06-06 12:07:28 +0530

    Fixed: the payment page link is generated with ssl as false in the admin order controller in the order page in back office

--- controllers/admin/AdminCartsController.php
+++ controllers/admin/AdminCartsController.php
@@ -935,7 +935,8 @@ class AdminCartsControllerCore extends AdminController
             'id_cart' => $id_cart,
             'order_message' => $message_content,
             'link_order' => $this->context->link->getPageLink(
-                'order', false,
+                'order',
+                null,
                 (int)$this->context->cart->id_lang,
                 'step=3&recover_cart='.$id_cart.'&token_cart='.md5(_COOKIE_KEY_.'recover_cart_'.$id_cart)
             ),
